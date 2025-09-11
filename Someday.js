26673a86bb976cc01132e4a46ed8b75d18e841cc
GashdanyHM <lchen.danny@gmail.com>
2025-09-11 00:12:07 -0700

    Update Someday.js

--- Someday.js
+++ Someday.js
@@ -41,44 +41,67 @@ let gameStatus = {};
 
 // --- 核心 p5.js 函数 ---
 function preload() {
-    // 您的所有美术、音频、字体资源加载代码都放在这里...
-    // (与您现有的文件保持一致即可)
+    // 【已修正所有文件名的大小写，请直接用这个函数替换你原来的 preload 函数】
+    
+    // UI 图像
     assets.ui_pause_normal = loadImage('assets/pauseBTN_normal.png');
     assets.ui_pause_hover = loadImage('assets/pauseBTN_hover.png');
     assets.ui_arrow_left_normal = loadImage('assets/left_arrow.png');
     assets.ui_arrow_left_hover = loadImage('assets/left_arrow_hover.png');
     assets.ui_arrow_right_normal = loadImage('assets/right_arrow.png');
     assets.ui_arrow_right_hover = loadImage('assets/right_arrow_hover.png');
-    assets.mainMenu_bg = loadImage('assets/gameCover.png');
-    assets.inbed_morning = loadImage('assets/sleepMorning.png');
-    assets.inbed_noon = loadImage('assets/sleepAfternoon.png');
-    assets.inbed_night = loadImage('assets/sleepNight.png');
-    assets.insideOptions = loadImage('assets/insideOptions.png');
-    assets.scene_bathroom = loadImage('assets/bathroomOptions.png');
-    assets.scene_cactus = loadImage('assets/cactusOptions.png');
-    assets.scene_window = loadImage('assets/windowOptions.png');
-    assets.scene_bed = loadImage('assets/bedOptions.png');
-    assets.scene_kitchen = loadImage('assets/kitchenOptions.png');
-    assets.scene_desk = loadImage('assets/deskOptions.png');
-    assets.scene_door = loadImage('assets/leavehomeOptions.png');
-    assets.scene_drowned_cactus = loadImage('assets/cactus_drowned.png');
-    assets.ui_close_icon_normal = null;
-    assets.ui_close_icon_hover = null;
-    assets.item_tnt_image = loadImage('assets/arrows.png');
+    assets.ui_close_icon_normal = null; // 保持不变
+    assets.ui_close_icon_hover = null;  // 保持不变
+
+    // 场景背景和 CG
+    assets.mainMenu_bg = loadImage('assets/GameCover.png'); // 修正: G, C
+    assets.inbed_morning = loadImage('assets/SleepMorning.png'); // 修正: S, M
+    assets.inbed_noon = loadImage('assets/SleepAfternoon.png'); // 修正: S, A
+    assets.inbed_night = loadImage('assets/SleepNight.png'); // 修正: S, N
+    assets.insideOptions = loadImage('assets/InsideOptions.png'); // 修正: I, O
+    assets.scene_bathroom = loadImage('assets/BathroomOptions.png'); // 修正: B, O
+    assets.scene_cactus = loadImage('assets/CactusOptions.png'); // 修正: C, O
+    assets.scene_window = loadImage('assets/WindowOptions.png'); // 修正: W, O
+    assets.scene_bed = loadImage('assets/BedOptions.png'); // 修正: B, O
+    assets.scene_kitchen = loadImage('assets/KitchenOptions.png'); // 修正: K, O
+    assets.scene_desk = loadImage('assets/DeskOptions.png'); // 修正: D, O
+    assets.scene_door = loadImage('assets/LeavehomeOptions.png'); // 修正: L, O
+    assets.scene_drowned_cactus = loadImage('assets/Cactus_drowned.png'); // 修正: C
+    assets.cg_getUp = loadImage('assets/cg_getup.png');
+    assets.cg_wing = loadImage('assets/cg_wing.png');
+    assets.fly_locationOptions = loadImage('assets/fly_location_select.png');
+    assets.scene_jumpedOut = loadImage('assets/jumpedOut.png');
+    assets.scene_hike = loadImage('assets/hike.png');
+    
+    // 商店相关
+    assets.scene_storeOutside = loadImage('assets/StoreOut.png'); // 修正: S, O
+    assets.scene_store_counter = loadImage('assets/store_buy.png');
+    assets.scene_store_front = loadImage('assets/store_front.png');
+    assets.scene_store_robbery = loadImage('assets/store_rob.png');
+
+    // 结局图像
+    assets.ending_eternalSleep = loadImage('assets/Ending1_BedForever.png'); // 修正: E, B, F
+    assets.ending_cactusDrowned = null; // 保持不变
+    assets.ending_didntGetUp = loadImage('assets/didntEvenTry.png'); // 修正: D, E, T
+    assets.ending_dieUnderStaff = loadImage('assets/ending_dieUnderStaff.png');
+    
+    // 物品图像 (注意：你的代码引用了 arrows.png，但仓库里没有这个文件，这是一个潜在问题)
+    assets.item_tnt_image = loadImage('assets/arrows.png'); 
     assets.item_potion_image = loadImage('assets/arrows.png');
     assets.item_blade_image = loadImage('assets/arrows.png');
+
+    // 视频 (视频文件通常较大，请确保它们已成功上传到 GitHub)
     assets.tv_news_normal = createVideo('assets/tv_news_normal.mp4');
-    assets.tv_news_crime = createVideo('assets/tv_news_normal.mp4');
+    assets.tv_news_crime = createVideo('assets/tv_news_normal.mp4'); // 注意这里引用了同一个文件
     assets.tv_news_normal.hide();
     assets.tv_news_crime.hide();
-    assets.ending_eternalSleep = loadImage('assets/Ending1_BedForever.png');
-    assets.ending_cactusDrowned = null;
-    assets.ending_didntGetUp = loadImage('assets/didntEvenTry.png');
-    assets.handwritingFont = loadFont('assets/MRF Lemonberry Sans.otf');
-    assets.cg_getUp = loadImage('assets/cg_getup.png');
-    //sounds
+
+    // 字体
+    assets.handwritingFont = loadFont('assets/MRF Lemonberry Sans.otf'); // 修正: M, R, F, L, S
+
+    // 音效 (Sounds)
     assets.bgm_start = loadSound('assets/sounds/alarm.mp3');
-    assets.sfx_loop = null;
+    assets.sfx_loop = null; // 保持不变
     assets.sfx_yawn = loadSound('assets/sounds/yawn.mp3');
     assets.sfx_click = loadSound('assets/sounds/mouseClicked.mp3');
     assets.sfx_cash = loadSound('assets/sounds/cash.mp3');
@@ -90,21 +113,6 @@ function preload() {
     assets.bgm_breeze = loadSound('assets/sounds/breeze.mp3');
     assets.sfx_storeBell = loadSound('assets/sounds/storeBell.mp3');
     assets.sfx_blanket = loadSound('assets/sounds/blanket.mp3');
-    //outside
-    assets.scene_storeOutside = loadImage('assets/storeOut.png');
-    assets.scene_store_counter = loadImage('assets/store_buy.png');
-    assets.scene_store_front = loadImage('assets/store_front.png');
-    assets.scene_store_robbery = loadImage('assets/store_rob.png');
-    assets.ending_dieUnderStaff = loadImage('assets/ending_dieUnderStaff.png');
-    assets.cg_wing = loadImage('assets/cg_wing.png');
-    assets.fly_locationOptions = loadImage('assets/fly_location_select.png');
-    assets.scene_jumpedOut = loadImage('assets/jumpedOut.png');
-    assets.scene_hike = loadImage('assets/hike.png');
-    assets.scene_amusement = loadImage('assets/park.png');
-    assets.scene_insidePark = loadImage('assets/insidePark.png');
-    assets.scene_meetInsectoid = loadImage('assets/meetInsectoid.png');
-    assets.ending_Sunset = loadImage('assets/EndingSunset.png');
-    assets.ending_toilet = loadImage('assets/toiletseat.png');
 }
 
 function setup() {
@@ -192,4 +200,5 @@ function loadScene(nodeId) {
         resetGameStatus();
     }
     sceneManager.load(nodeId);
+
 }
